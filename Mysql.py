b926a0744ef6cd4199a1b33b65e40377801e586c
Mike <89531276499nokia@gmail.com>
2021-10-31 10:43:10 +0300

    if / else

--- Mysql.py
+++ Mysql.py
@@ -7,5 +7,16 @@ sql.execute("CREATE TABLE IF NOT EXISTS users("
             "login TEXT,"
             "password TEXT,"
             "cash INT)")
-
 db.commit()
+
+user_login = input('Введите логин: ')
+user_password = input('Введите пароль: ')
+
+sql.execute("SELECT login FROM users")
+if sql.fetchone() is None:
+    sql.execute("INSERT INTO users (login, password, cash) VALUES (?, ?, ?)",
+            (user_login, user_password, 0))
+    db.commit()
+else:
+    print(f'{user_login} у нас уже есть!')
+
